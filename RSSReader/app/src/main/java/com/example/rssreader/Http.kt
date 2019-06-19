c831d651436d006c2c8be47474ed9a92d8c2430b
Kazuki -Kimura <kazukidddd836@gmail.com>
2019-06-19 22:57:43 +0900

    no message

--- RSSReader/app/src/main/java/com/example/rssreader/Http.kt
+++ RSSReader/app/src/main/java/com/example/rssreader/Http.kt
@@ -7,10 +7,10 @@ import javax.net.ssl.HttpsURLConnection
 
 fun httpGet(url: String): InputStream? {
 
-    //
+    //通信接続用のオブジェクトを作る
     val con = URL(url).openConnection() as HttpsURLConnection
 
-    //
+    //接続の設定を行う
     con.apply {
         requestMethod = "GET"
         connectTimeout = 3000
@@ -18,15 +18,15 @@ fun httpGet(url: String): InputStream? {
         instanceFollowRedirects = true
     }
 
-    //
+    //接続する
     con.connect()
 
-    //
+    //ステータスコードの確認
     if(con.responseCode in 200..299){
-        //
+        //成功したら、レスポンスの入力ストリームを、BufferedInputStreamとして返す
         return BufferedInputStream(con.inputStream)
     }
 
-    //
+    //失敗
     return null
 }
--- RSSReader/app/src/main/java/com/example/rssreader/MainActivity.kt
+++ RSSReader/app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -9,33 +9,34 @@ import android.support.v4.content.Loader
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(),LoaderManager.LoaderCallbacks<Rss> {
+
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Rss> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //
+        //ローダーを呼び出す
         loaderManager.initLoader(1,null,this)
     }
 
-    //
+    //ローダーが要求されたときに呼ばれる
     override fun onCreateLoader(id:Int,args:Bundle?) = RssLoader(this)
 
-    //
+    //ローダーがリセットされたときに呼ばれる
     override fun onLoaderReset(loader: Loader<Rss>) {
         //特に何もしない
     }
 
-    //
+    //ローダーで行った処理が終了したときに呼ばれる
     override fun onLoadFinished(loader: Loader<Rss>, data: Rss?) {
-        //
+        //処理結果がnullではない場合
         if (data != null){
 
-            //
+            //RecyclerViewをレイアウトから探す
             val recyclerView = findViewById<RecyclerView>(R.id.articles)
 
-            //
+            //RSSの記事一覧のアダプター
             val adapter = ArticlesAdapter(this,data.articles){ article ->
                 //記事をタップしたときの処理
                 val intent = CustomTabsIntent.Builder().build()
@@ -43,7 +44,7 @@ class MainActivity : AppCompatActivity(),LoaderManager.LoaderCallbacks<Rss> {
             }
             recyclerView.adapter = adapter
 
-            //
+            //グリッド表示するレイアウトマネージャ
             val layoutManager = GridLayoutManager(this,2)
 
             recyclerView.layoutManager = layoutManager
