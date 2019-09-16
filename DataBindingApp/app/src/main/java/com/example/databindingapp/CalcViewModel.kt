a360c76782475c165aa0f96d12cd7fbb0b0f9cae
Kazuki -Kimura <kazukidddd836@gmail.com>
2019-09-17 07:35:08 +0900

    修正

--- DataBindingApp/app/src/main/java/com/example/databindingapp/CalcViewModel.kt
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/CalcViewModel.kt
@@ -1,9 +1,67 @@
 package com.example.databindingapp
 
-class CalcViewModel {
-    var result = "0"
+import androidx.databinding.ObservableField
 
-    fun buttonClicked(number :Int){
-        result = number.toString()
+class CalcViewModel {
+    var result = ObservableField("")
+    var mValueOne = 0.0F
+    var mValueTwo = 0.0F
+    var isPlus = false
+    var isMinus = false
+    var isOnce = false
+
+    fun buttonClicked(number : Int){
+        val builder = StringBuilder()
+        builder.append(result.get()).append(number.toString())
+        result.set(builder.toString())
+    }
+
+    fun dotClicked(){
+        val regex = result.get()?.indexOf(".")
+        val builder = StringBuilder()
+        if (regex == -1){
+            builder.append(result.get()).append(".")
+            result.set(builder.toString())
+        }
+    }
+
+    fun clearClicked(){
+        result.set("")
+        isOnce = false
+    }
+
+    fun plusClicked(){
+        if (result.get() != ""){
+            mValueOne = result.get()?.toFloat() ?: 0.0F
+            isPlus = true
+            result.set("")
+        }
+    }
+
+    fun minusClicked(){
+        if (result.get() != ""){
+            mValueOne = result.get()?.toFloat() ?: 0.0F
+            isMinus = true
+            result.set("")
+        }
+    }
+
+    fun equalClicked(){
+        if (result.get() != ""){
+            if (!isOnce){
+                mValueTwo = result.get()?.toFloat() ?: 0.0F
+                isOnce = true
+            }
+
+            if (isPlus){
+                var resultAll = mValueOne + mValueTwo
+                mValueOne = mValueTwo
+                result.set(resultAll.toString())
+            }else if (isMinus){
+                var resultAll = mValueOne - mValueTwo
+                result.set(resultAll.toString())
+            }
+        }
+
     }
 }
