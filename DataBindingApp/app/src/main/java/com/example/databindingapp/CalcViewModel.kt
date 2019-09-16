b3c668414c10121ff13255c0fb35c367200dc5b5
Kazuki -Kimura <kazukidddd836@gmail.com>
2019-09-16 13:55:55 +0900

    databinding準備

--- DataBindingApp/app/src/main/java/com/example/databindingapp/CalcViewModel.kt
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/CalcViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.databindingapp
 
 class CalcViewModel {
+    var result = "0"
+
+    fun buttonClicked(number :Int){
+        result = number.toString()
+    }
 }
--- DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.kt
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.databindingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.databinding.DataBindingUtil
+import com.example.databindingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.data = CalcViewModel()
     }
 }
